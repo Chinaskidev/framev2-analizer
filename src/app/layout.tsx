5f4ee29a22ac1ed19c7f96a2ad2914d6ec10af1b
Chinaskidev <chinaskicharles0@gmail.com>
2025-03-11 13:36:53 -0600

    agregando metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,25 @@ import type { Metadata } from "next";
 import "../app/globals.css";
 import { Providers } from "../app/providers";
 
+
 export const metadata: Metadata = {
-  title: "Analizador de C.V",
-  description: "A Farcaster Frames v2 demo app",
+  metadataBase: new URL('https://framev2-analizer.vercel.app/'),
+  title: 'Mi Frame - Analizador de Curriculums',
+  description: 'Recibe feedback inteligente sobre tu curriculum usando IA.',
+  openGraph: {
+    title: 'Mi Frame - Analizador de Curriculums',
+    description: 'Recibe feedback inteligente sobre tu curriculum usando IA.',
+    images: [
+      {
+        url: 'https://framev2-analizer.vercel.app/skinner-logo5.png', // imagen desde vercel
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+  other: {
+    'farcaster:frame': 'true',
+  },
 };
 
 export default function RootLayout({
